f5c59805f338a238c820a4f1a73920d90362a82b
shadrinams <shadrinams@gmail.com>
2017-03-22 15:22:27 -0400

    Add files via upload

---
+++ gyration.tcl
@@ -0,0 +1,30 @@
+proc cut_ca {name type} {
+
+set root "/../"
+cd "$root/$name/"
+set file [open "gyration_[list $type].dat" w]
+puts $file "time|radius of gyration"
+puts $file "ps|A"
+
+
+mol new [list $type]_prot.psf
+mol addfile [list $type]_prot.pdb
+mol addfile md_[list $type]_whole.xtc waitfor all step 5
+
+#mkdir CA
+
+set sel [atomselect top "protein and noh"]
+set nf [molinfo top get numframes]
+
+for {set i 0} {$i<$nf} {incr i} {
+	$sel frame $i
+	set gyr [measure rgyr $sel weight mass]
+	set time [expr ($i*10)]
+	puts $file "$time|$gyr"
+	}
+
+mol delete top
+close $file
+
+}
+
